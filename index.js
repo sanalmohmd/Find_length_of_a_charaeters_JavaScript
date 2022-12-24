45ada7923d65072d5de2db3cb2fdbc8a0843c13e
sanalmohmd <sanalmuhammed12@gmail.com>
2022-12-24 12:33:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// var word = document.querySelector(".word-input");
+
+// var result = document.querySelector(".length-get");
+
+
+// let len = word.value.length;
+
+
+
+// result.innerHTML = len;
+
+
+
+function show(){
+
+    var text = document.getElementById("text").value;
+
+
+    document.getElementById("length-get").innerHTML= ("length : ") + text.length;
+}
